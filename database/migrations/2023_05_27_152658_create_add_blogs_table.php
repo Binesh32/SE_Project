1f0bcd8064efee5da2ffb6ceb666b5c5a12bcd87
Ganesh <ganesh@gmail.com>
2023-05-29 10:54:32 +0545

    pulle

--- database/migrations/2023_05_27_152658_create_add_blogs_table.php
+++ database/migrations/2023_05_27_152658_create_add_blogs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('add_blogs', function (Blueprint $table) {
             $table->id();
-            $table->int('admin_id');
+            $table->integer('admin_id');
             $table->string('title');
             $table->string('message');
             $table->string('image');
